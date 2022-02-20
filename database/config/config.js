d84d1e81144d68bd04ef3ada7c25b23a9a025d12
Gitolite Okaski <me@gibzzz.tk>
2022-02-20 08:09:26 +0300

    Production config

--- database/config/config.js
+++ database/config/config.js
@@ -5,12 +5,7 @@ config();
 export default {
     development: {
         url: process.env.DATABASE_URL,
-        dialect: "postgres",
-        dialectOptions: {
-            ssl: {
-                rejectUnauthorized: false
-            }
-        }
+        dialect: "postgres"
     },
     production: {
         url: process.env.DATABASE_URL,
